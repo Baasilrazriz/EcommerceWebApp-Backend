fb784bfafaba2c85297a5186c5558713c268a7ca
shahoodrehan <shahoodrehan57@gmail.com>
2023-12-12 00:39:07 +0500

    Create User with hashing

--- EcommerceWebApplication/Service/CreateUser.cs
+++ EcommerceWebApplication/Service/CreateUser.cs
@@ -1,6 +1,8 @@
 ﻿using EcommerceWebApplication.Data;
 using EcommerceWebApplication.Models;
 using EcommerceWebApplication.Utilities;
+using System;
+using System.Threading.Tasks;
 
 namespace EcommerceWebApplication.Service
 {
@@ -12,12 +14,20 @@ namespace EcommerceWebApplication.Service
         {
             _context = context;
         }
+        //create user is working perfectly with hashing
         public async Task<UserModel> CreateNewUserAsync(UserDto userDto)
         {
             using (var transaction = await _context.Database.BeginTransactionAsync())
             {
                 try
                 {
+                    byte[] imageBytes = null;
+                    if (!string.IsNullOrWhiteSpace(Convert.ToBase64String(userDto.image)))
+                    {
+                        // Convert the base64-encoded string to a byte array
+                        imageBytes = Convert.FromBase64String(Convert.ToBase64String(userDto.image));
+                    }
+
                     var newUser = new UserModel
                     {
                         FirstName = userDto.FirstName,
@@ -33,8 +43,16 @@ namespace EcommerceWebApplication.Service
                         role = userDto.role,
                         username = userDto.username,
                         password = PasswordHasher.HashPassword(userDto.password),
-                        image = userDto.image
+                        image = imageBytes // Assign the byte array here
                     };
+                    var newuser = new ApplicationUsers
+                    {
+                        UserName = userDto.username,
+                        Email = userDto.email,
+                        Password = PasswordHasher.HashPassword(userDto.password),
+
+                    };
+                    _context.ApplicationUsers.Add(newuser);
                     _context.UserModels.Add(newUser);
 
                     // Save changes in the context to the database
@@ -47,13 +65,12 @@ namespace EcommerceWebApplication.Service
                 }
                 catch (Exception ex)
                 {
+                    // Rollback the transaction if any exception occurs
                     await transaction.RollbackAsync();
-                    throw;
+                    // Throw an application-specific exception or handle the error as appropriate for your application
+                    throw new ApplicationException("An error occurred when creating a new user.", ex);
                 }
-
             }
-
-
         }
     }
 }
