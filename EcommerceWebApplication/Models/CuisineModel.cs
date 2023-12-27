989e450ec3d688574e0eb12476c07e7561372bb5
shahoodrehan <shahoodrehan57@gmail.com>
2023-12-27 11:30:55 +0500

    CuisineModel

---
+++ EcommerceWebApplication/Models/CuisineModel.cs
@@ -0,0 +1,15 @@
+﻿namespace EcommerceWebApplication.Models
+{
+    public class CuisineModel
+    {
+        public int CuisineId { get; set; }
+        public int RestaurantId { get; set; }
+        public string Cuisinetype { get; set; }
+        public string CuisineName { get; set; }
+        public string CuisineImage { get; set; }
+        public double Price { get; set; }
+        public string Description { get; set; }
+        public int Stock { get; set; }
+
+    }
+}
--- EcommerceWebApplication/Models/RestaurantModel.cs
+++ EcommerceWebApplication/Models/RestaurantModel.cs
@@ -1,6 +1,16 @@
-﻿namespace EcommerceWebApplication.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EcommerceWebApplication.Models
 {
     public class RestaurantModel
     {
+        [Required]
+        public int RestaurantId { get; set;}
+        public string RestaurantName { get; set;}
+        public string Image {  get; set;}
+        public double Deliverytime { get; set;}
+        public string Description { get; set;}
+        public double Rating { get; set;}
+        public string RestaurantCategory { get; set;}
     }
 }
